691a6c85fce4d28f798faffa185d514e46ce7b41
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-03-10 09:06:10 -0600

    Save corrections

---
+++ src/App/AppUI.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import { TodoCounter } from "../TodoCounter";
+import { TodoItem } from "../TodoItem";
+import { TodoList } from "../TodoList";
+import { TodoSearch } from "../TodoSearch";
+import { CreateTodoButton } from "../CreateTodoButton";
+
+function AppUI({
+	loading,
+	error,
+	totalTodos,
+	completedTodos,
+	searchValue,
+	setSearchValue,
+	searchedTodos,
+	completeTodo,
+	deleteTodo
+}) {
+	return (
+		<React.Fragment>
+			<TodoCounter
+				total={totalTodos}
+				completed={completedTodos}
+			/>
+			<TodoSearch
+				searchValue={searchValue}
+				setSearchValue={setSearchValue}
+			/>
+
+			<TodoList>
+				{error && <p>Desesperate, hubo un error...</p>}
+				{loading && <p>Estamos cargando, no desesperes...</p>}
+				{(!loading && !searchedTodos.length && !error) && <p>Crea tu primer TODO!</p>}
+
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+						completed={todo.completed}
+						onComplete={() => completeTodo(todo.text)}
+						onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+	)
+}
+
+export { AppUI };
